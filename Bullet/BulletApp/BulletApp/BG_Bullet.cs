0b449b9b389b7bb58de30264479dc84c653a06d8
LucienCamuglia <lucien.camuglia@gmail.com>
2017-03-02 10:26:06 +0100

    Création du projet BulletApp

---
+++ Bullet/BulletApp/BulletApp/BG_Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletApp
+{
+    class BG_Bullet
+    {
+        public float VelocityX { get; set; }
+        public float VelocityX { get; set; }
+
+        private int x, y;
+
+    }
+}
