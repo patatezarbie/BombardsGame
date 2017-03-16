9c35c52626d65bd0ed2265e6eecb7c4ef64d348d
Faeron <loris.debiasi@gmail.com>
2017-03-16 09:02:31 +0100

    Commenting the RichTextBox class
    + Comment

--- Interface/Bomber_InterfaceGraphique/Bomber_InterfaceGraphique/BG_RichTextBoxExtensions.cs
+++ Interface/Bomber_InterfaceGraphique/Bomber_InterfaceGraphique/BG_RichTextBoxExtensions.cs
@@ -7,6 +7,12 @@ namespace Bomber_InterfaceGraphique
 {
     public static class BG_RichTextBoxExtensions
     {
+        /// <summary>
+        /// Add a string with a certain color to a RichTextBox
+        /// </summary>
+        /// <param name="box"></param>
+        /// <param name="text"></param>
+        /// <param name="color"></param>
         public static void AppendText(this RichTextBox box, string text, Color color)
         {
             box.SelectionStart = box.TextLength;
@@ -17,18 +23,26 @@ namespace Bomber_InterfaceGraphique
             box.SelectionColor = box.ForeColor;
         }
 
+        /// <summary>
+        /// Write the scoreboard in a RichTextBox and randomize the color of each line except the first one
+        /// </summary>
+        /// <param name="box">The RichTextBox</param>
+        /// <param name="playerList">The list of player</param>
         public static void WriteScore(this RichTextBox box, List<Player> playerList)
         {
-            //Need to sort the list by score before
+            //NEED TO SORT THE LIST BY SCORE BEFORE
 
+            //Clear the list before showing the scoreboard
             box.Clear();
             Random rnd = new Random();
 
+            //Add the text "Score" as first line
             box.AppendText("Score" + Environment.NewLine + Environment.NewLine);
+            //Show the name and the score of each player
             foreach (var player in playerList)
             {
                 Color randomColor = Color.FromArgb(rnd.Next(128), rnd.Next(128), rnd.Next(128));
-                //Ajouter le score (player : score)
+                //NEED TO ADD THE SCORE [Player class needed]
                 box.AppendText(player.name + " : 10" + Environment.NewLine, randomColor);
             }
         }
